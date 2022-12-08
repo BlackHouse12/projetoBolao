3f23151a0c3f267d574a0724f12c53c30315bb2f
BlackHouse12 <101483364+BlackHouse12@users.noreply.github.com>
2022-12-08 20:04:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,53 @@
+index.php
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+require_once 'usuarios.php';
+$u = new Usuarios;
+
+
+
+
 ?>
-Something is wrong with the XAMPP installation :-(
+
+
+<html lang="pt-br">
+<head>
+    <meta charset = "utf-8"/>
+    <title>Projeto Login</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+<div id="corpo-form">
+    <h1>Entrar</h1>
+    <form method = "POST" action="processa.php">
+        <input type="email" placeholder="Usuario" nome="nome">
+        <input type="passaword" placeholder="senha" nome="senha">
+        <input type="submit" value="Acessar">
+        <a href="cadastrar.php" >ainda não é inscrito?<strong>Cadastre-se<strong></a>
+</form>
+</div>
+
+<?php
+if (isset($_POST['nome']))
+{
+    $email = addslashes($_POST['email']);
+    $senha = addslashes($_POST['senha']);
+
+    if(!empty($email) && !empty($senha))
+    {
+        
+
+    }else{
+        echo "Preencha todos os campos!";
+    }
+
+
+
+
+
+
+
+?>
+
+
+</body>
